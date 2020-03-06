951fae4f67f067cc6a0f7027f10545a9f843e85c
alexfrances0606 <60611192+alexfrances0606@users.noreply.github.com>
2020-03-06 17:07:13 +0100

    carte réalisé
    création de la carte et du marqueur

---
+++ script.js
@@ -0,0 +1,31 @@
+// votre code JS
+
+var mymap = L.map('mapid').setView([48.853, 2.35], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoianVsaWVua29tcCIsImEiOiJjanR1NGFuYjkxMmNvNDBucGI1aXZ4Y285In0.hiSplFD5CODUd9yxRO_qkg'
+
+}).addTo(mymap);
+
+
+var marker = L.marker([48.858370, 2.294481]).addTo(mymap);
+
+
+var circle = L.circle([48.858370, 2.294481], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
+
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(mymap);
+
